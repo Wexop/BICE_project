d06a2270577337987f8c31349faaec3cfb971cde
Wexop <mattheurtin@gmail.com>
2023-04-13 15:56:36 +0200

    #35 start materiel SRV

---
+++ BICE.SRV/Materiel_SRV.cs
@@ -0,0 +1,52 @@
+﻿using BICE.DTO;
+using BICE.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometrie.DAL;
+
+namespace BICE.SRV
+{
+    internal class Materiel_SRV<T> : IMateriel_SRV<T>
+        where T : Materiel_DTO
+    {
+        // champs
+
+        protected IDepot_DAL<Materiel_DAL> depot;
+        public Materiel_SRV(IDepot_DAL<Materiel_DAL> dpt)
+        {
+            this.depot = dpt;
+        }
+
+        public Materiel_SRV() : this(new Materiel_Depot_DAL()) { }
+
+        // methodes privées
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Modifier(T m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Supprimer(Materiel_DTO m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Ajouter(T m)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
