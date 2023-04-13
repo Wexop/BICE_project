60c95cb3b16e4bd55832f2f368fbaa63fe2dfe1b
Wexop <mattheurtin@gmail.com>
2023-04-13 11:43:19 +0200

    create intervention DAL

---
+++ BICE.DAL/Intervention_DAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DAL
+{
+    internal class Intervention_DAL
+    {
+    }
+}
