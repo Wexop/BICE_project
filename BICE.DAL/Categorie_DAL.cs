4e73e2ca3fb62607cff0b96db5654802c1ad8b4b
Wexop <mattheurtin@gmail.com>
2023-04-13 13:10:34 +0200

    add categorie DAL

---
+++ BICE.DAL/Categorie_DAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DAL
+{
+    internal class Categorie_DAL
+    {
+        public int Id_categorie { get; set; }
+        public string Nom { get; set; }
+
+        public Categorie_DAL(int id_categorie, string nom) 
+            => (Id_categorie, Nom) = (id_categorie, nom); 
+    }
+}
