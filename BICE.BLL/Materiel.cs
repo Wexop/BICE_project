3f4ca185d91bd4621e8ae6fb39fdf5ac71f0b233
Wexop <mattheurtin@gmail.com>
2023-05-03 10:31:38 +0200

    #50 make private set for BLL object

--- BICE.BLL/Materiel.cs
+++ BICE.BLL/Materiel.cs
@@ -2,15 +2,15 @@
 {
     public class Materiel
     {
-        public string CodeBarre { get; set; }
-        public string Nom { get; set; }
-        public int Categorie_ID { get; set; }
-        public int? Nb_utilisation { get; set; }
-        public int? Nb_utilisation_max { get; set; }
-        public DateTime? Date_peremption { get; set; }
-        public DateTime? Date_controle { get; set; }
-        public DateTime? Date_prochain_controle { get; set; }
-        public bool Stock { get; set; }
+        public string CodeBarre { get; private set; }
+        public string Nom { get; private set; }
+        public int Categorie_ID { get; private set; }
+        public int? Nb_utilisation { get; private set; }
+        public int? Nb_utilisation_max { get; private set; }
+        public DateTime? Date_peremption { get; private set; }
+        public DateTime? Date_controle { get; private set; }
+        public DateTime? Date_prochain_controle { get; private set; }
+        public bool Stock { get; private set; }
 
         // constructeur
         public Materiel(string codeBarre, string nom, int categorieId, int? nbUtilisation, int? nbUtilisationMax, DateTime? datePeremption, DateTime? dateControle, DateTime? dateProchainControle, bool stock)
