98db496d4ba4db97c1ada20bda55fe1a1c10475e
Wexop <mattheurtin@gmail.com>
2023-05-04 14:03:17 +0200

    add materielIntervention DTO and VehiculeIntervention DTO

---
+++ BICE.DTO/MaterielIntervention_DTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DTO
+{
+    public class MaterielIntervention_DTO
+    {
+        public int IdVehiculeIntervention { get; set; }
+        public string CodeBarreMateriel { get; set; }
+    }
+}
---
+++ BICE.DTO/VehiculeIntervention_DTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DTO
+{
+    internal class VehiculeIntervention_DTO
+    {
+        public int IdVehiculeIntervention { get; set; }
+        public int idIntervention { get; set; }
+        public string immatriculationVehicule { get; set; }
+    }
+}
