7e97c72b44022b7e035d7798282f4b71257210ac
Wexop <mattheurtin@gmail.com>
2023-05-04 14:10:35 +0200

    #67 add materiel intervention BLL and vehicule intervention BLL

---
+++ BICE.BLL/MaterielIntervention.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.BLL
+{
+    public class MaterielIntervention
+    {
+        public int IdVehiculeIntervention { get; set; }
+        public string CodeBarreMateriel { get; set; }
+
+        public MaterielIntervention (int idVehiculeIntervention, string codeBarre)
+        {
+            IdVehiculeIntervention= idVehiculeIntervention;
+            CodeBarreMateriel= codeBarre;
+        }
+    }
+}
---
+++ BICE.BLL/VehiculeIntervention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.BLL
+{
+    public class VehiculeIntervention
+    {
+        public int IdVehiculeIntervention { get; set; }
+        public int IdIntervention { get; set; }
+        public string ImmatriculationVehicule { get; set; }
+
+        public VehiculeIntervention(int idVehiculeIntervention, int idIntervention, string immatriculation)
+        { 
+            IdVehiculeIntervention= idVehiculeIntervention;
+            IdIntervention= idIntervention;
+            ImmatriculationVehicule= immatriculation;
+        }
+    }
+}
