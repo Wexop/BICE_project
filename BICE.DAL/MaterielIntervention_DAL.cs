032ce39706de6d8e26f7dc520d949691b9d4300c
Wexop <mattheurtin@gmail.com>
2023-05-04 14:16:14 +0200

    #61 add materielIntervention DAL and vehiculeInterventionDAL

---
+++ BICE.DAL/MaterielIntervention_DAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DAL
+{
+    public class MaterielIntervention_DAL
+    {
+        public int IdVehiculeIntervention { get; set; }
+        public string CodeBarreMateriel { get; set; }
+
+        public MaterielIntervention_DAL(int idVehiculeIntervention, string codeBarre)
+            => (IdVehiculeIntervention, CodeBarreMateriel) = (idVehiculeIntervention, codeBarre);
+    }
+}
---
+++ BICE.DAL/VehiculeIntervention_DAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DAL
+{
+    internal class VehiculeIntervention_DAL
+    {
+        public int IdVehiculeIntervention { get; set; }
+        public int IdIntervention { get; set; }
+        public string ImmatriculationVehicule { get; set; }
+
+        public VehiculeIntervention_DAL(int idVehiculeIntervention, int idIntervention, string immatriculationVehicule)
+            => (IdVehiculeIntervention, IdIntervention, ImmatriculationVehicule) = (idVehiculeIntervention, idIntervention, immatriculationVehicule);
+    }
+}
